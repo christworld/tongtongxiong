c2f5d8149b3bebde53a87606d0d4b0eeb448540f
christworld <christworld@users.noreply.github.com>
2018-01-30 23:02:35 +0800

    添加提示音
    添加提示音

--- app/src/main/java/com/allbear/hopedemo/SingleTask/Turn.java
+++ app/src/main/java/com/allbear/hopedemo/SingleTask/Turn.java
@@ -22,6 +22,6 @@ public class Turn extends SingleTaskBase {
     @Override
     public void doProgramTime() {
         Log.info(LogTag, "doProgramTime");
-
+        mMediaPlayer.startPlay("Sound/wrong.wav");
     }
 }
